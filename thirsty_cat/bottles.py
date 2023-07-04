c8cec542d00436b3dd86bfd662de30f5e219d102
Bruce Stull <bruce.stull@gmail.com>
2023-07-04 06:29:58 -0400

    Added docstrings

--- thirsty_cat/bottles.py
+++ thirsty_cat/bottles.py
@@ -1,14 +1,32 @@
 class Bottle:
+    """
+    Class representing a bottle.
+
+    Attributes:
+        capacity (int): The maximum volume of the bottle.
+        current_volume (int): The current volume of the bottle.
+
+    Methods:
+        fill: Adds liquid to the bottle.
+        empty: Empties the bottle.
+    """
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.current_volume = 0
 
     def fill(self, amount):
+        """
+        Method for adding liquid to the bottle.
+        """
         if self.current_volume + amount > self.capacity:
             raise ValueError("Too much liquid!")
         self.current_volume += amount
 
     def empty(self):
+        """
+        Method for emptying the bottle.
+        """
         self.current_volume = 0
 
     def __str__(self):
@@ -16,6 +34,19 @@ class Bottle:
 
 
 class PopBottle(Bottle):
+    """
+    Class representing a pop bottle. This class inherits from the Bottle class.
+
+    Attributes:
+        capacity (int) [from super class]: The maximum volume of the bottle.
+        flavor (str): The flavor of the pop.
+        brand (str): The brand of the pop.
+
+    Methods:
+        fill [from super class]: Adds liquid to the bottle.
+        empty [from super class]: Empties the bottle.
+    """
+
     def __init__(self, capacity, flavor, brand):
         super().__init__(capacity)
         self.flavor = flavor
@@ -26,6 +57,19 @@ class PopBottle(Bottle):
 
 
 class BeerBottle(Bottle):
+    """
+    Class representing a beer bottle. This class inherits from the Bottle class.
+
+    Attributes:
+        capacity (int) [from super class]: The maximum volume of the bottle.
+        flavor (str): The flavor of the beer.
+        brand (str): The brand of the beer.
+
+    Methods:
+        fill [from super class]: Adds liquid to the bottle.
+        empty [from super class]: Empties the bottle.
+    """
+
     def __init__(self, capacity, flavor, brand):
         super().__init__(capacity)
         self.flavor = flavor
